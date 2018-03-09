533d3c3d6af3582e3326a3cce48aeb712a15cf27
kurs <kurs@kurs-Komputer>
2018-03-09 11:58:53 +0100

    trzecia

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -5,6 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Pierwsza wersja");
 		System.out.println("Druga wersja");
+		System.out.println("Trzecia wersja");
 	}
 
 }
